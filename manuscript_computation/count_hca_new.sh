a8ac7454f0a42d1ccc8b1ec7c8c80fdcc47ef191
mtcarilli <68964724+mtcarilli@users.noreply.github.com>
2025-03-17 10:20:34 -0700

    Add files via upload

---
+++ manuscript_computation/count_hca_new.sh
@@ -0,0 +1,50 @@
+kb count --verbose -i /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/index.idx \
+-g /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/t2g.txt -x 10xv3 \
+-o ./U12 -t 10 -m 30G \
+-c1 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/cdna_t2c.txt \
+-c2 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/nascent_t2c.txt \
+--workflow nac --sum cell --overwrite --loom \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039368_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039368_3.fastq & \
+
+kb count --verbose -i /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/index.idx \
+-g /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/t2g.txt -x 10xv3 \
+-o ./U14 -t 10 -m 30G \
+-c1 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/cdna_t2c.txt \
+-c2 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/nascent_t2c.txt \
+--workflow nac --sum cell --overwrite --loom \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039352_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039352_3.fastq & \
+
+kb count --verbose -i /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/index.idx \
+-g /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/t2g.txt -x 10xv3 \
+-o ./T12 -t 10 -m 30G \
+-c1 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/cdna_t2c.txt \
+-c2 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/nascent_t2c.txt \
+--workflow nac --sum cell --overwrite --loom \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039324_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039324_3.fastq \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039325_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039325_3.fastq & \
+
+kb count --verbose -i /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/index.idx \
+-g /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/t2g.txt -x 10xv3 \
+-o ./T14 -t 10 -m 30G \
+-c1 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/cdna_t2c.txt \
+-c2 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/nascent_t2c.txt \
+--workflow nac --sum cell --overwrite --loom \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039319_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039319_3.fastq \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039320_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039320_3.fastq & \
+
+kb count --verbose -i /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/index.idx \
+-g /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/t2g.txt -x 10xv3 \
+-o ./T1 -t 10 -m 30G \
+-c1 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/cdna_t2c.txt \
+-c2 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/nascent_t2c.txt \
+--workflow nac --sum cell --overwrite --loom \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039343_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039343_3.fastq \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039344_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039344_3.fastq & \
+
+kb count --verbose -i /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/index.idx \
+-g /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/t2g.txt -x 10xv3 \
+-o ./T17 -t 10 -m 30G \
+-c1 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/cdna_t2c.txt \
+-c2 /home/tchari/metadata/refdata-gex-GRCh38-2020-A/kallisto_nac_28/nascent_t2c.txt \
+--workflow nac --sum cell --overwrite --loom \
+/home/tchari/counts/monod_rev_HCA/fastq/SRR19039315_2.fastq /home/tchari/counts/monod_rev_HCA/fastq/SRR19039315_3.fastq
